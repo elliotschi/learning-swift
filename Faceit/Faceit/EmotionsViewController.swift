3b12ffef6f57ae1839d2e0036d6d0143db9b1b2b
Elliot S Chi <elliotschi@gmail.com>
2016-07-25 00:59:43 -0700

    feat: 2 mvc's

---
+++ Faceit/Faceit/EmotionsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  EmotionsViewController.swift
+//  Faceit
+//
+//  Created by elli chi on 7/25/16.
+//  Copyright © 2016 elli chi. All rights reserved.
+//
+
+import UIKit
+
+class EmotionsViewController: UIViewController {
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
